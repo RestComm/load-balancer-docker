04cde776407d90c2ad0e66a2ceb7841f0b5c50c7
Gennadiy Dubina <gdubina@dataart.com>
2016-03-14 16:29:37 +0200

    support amazon ec2, add prod_mode, improve logs conf

--- scripts/configure_load_balancer.sh
+++ scripts/configure_load_balancer.sh
@@ -32,6 +32,16 @@ if [ -n "${HOST_ADDRESS}" ]; then
     sed -i "s/host=.*/host=${HOST_ADDRESS}/" $LOADER_BALANCER_CONFIG
 fi
 
+if [ -n "${AMAZON_EC2}" ] && [ -z "${PUBLIC_IP}" ]; then
+    echo "Get real public ip for Amazon"
+    # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-instance-addressing.html
+    PUBLIC_IP=`curl -s -m 5 http://169.254.169.254/latest/meta-data/public-ipv4`
+    if [[ "$PUBLIC_IP" == *"404 Not Found"* ]]
+    then
+        PUBLIC_IP=""
+    fi
+fi
+
 if [ -n "${PUBLIC_IP}" ]; then
     echo "PUBLIC_IP : ${PUBLIC_IP}"
     sed -i "s/#public-ip=.*/public-ip=${PUBLIC_IP}/" $LOADER_BALANCER_CONFIG
@@ -51,3 +61,21 @@ if [ -z "${LOG_LEVEL}" ]; then
     echo "Setup default log level"
     LOG_LEVEL="INFO"
 fi
+
+LOG_LEVEL_INT=0
+
+case "$LOG_LEVEL" in
+    OFF ) LOG_LEVEL_INT=0;;
+    FATAL ) LOG_LEVEL_INT=100;;
+    ERROR ) LOG_LEVEL_INT=200;;
+    WARN ) LOG_LEVEL_INT=300;;
+    INFO ) LOG_LEVEL_INT=400;;
+    DEBUG ) LOG_LEVEL_INT=500;;
+    TRACE ) LOG_LEVEL_INT=600;;
+esac
+
+echo "Setup gov.nist TRACE_LEVEL: ${LOG_LEVEL_INT}"
+sed -i "s/gov.nist.javax.sip.TRACE_LEVEL=.*/gov.nist.javax.sip.TRACE_LEVEL=${LOG_LEVEL_INT}/" $LOADER_BALANCER_CONFIG
+
+echo "Setup log level: ${LOG_LEVEL}"
+sed -i "s/WARN/${LOG_LEVEL}/g" ${LOG4J_CONFIG}
