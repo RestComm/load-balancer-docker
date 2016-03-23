2a5869cf9194382a8cf2ed0e75940b23808e7820
Gennadiy Dubina <gdubina@dataart.com>
2016-03-23 17:39:53 +0000

    check if PROD_MODE is true

--- files/start_loadbalancer.sh
+++ files/start_loadbalancer.sh
@@ -2,7 +2,7 @@
 
 LB_JAVA_OPT=''
 
-if [ -n "$PROD_MODE" ]; then
+if [ "${PROD_MODE^^}" = "TRUE" ]; then
     LB_JAVA_OPT='-Xms2048m -Xmx2048m -XX:PermSize=512M -XX:MaxPermSize=1024M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false'
 fi
 
