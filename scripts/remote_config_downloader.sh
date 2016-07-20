1aab60d8f1838a0e62611fcbad61200e5f4a6a66
leftyb <Lefty1978!>
2016-07-20 15:54:36 +0300

    Fix issue #13

--- scripts/remote_config_downloader.sh
+++ scripts/remote_config_downloader.sh
@@ -2,7 +2,27 @@
 
 echo "Remote config downloader"
 
+function download_conf(){
+echo "url $1 $2 $3"
+if [[ `wget -S --spider $1 $2 $3 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+               if [ -n "$2" ] && [ -n "$3" ]; then
+                    wget $1 $2 $3 -O $4
+               else
+                    wget $1 -O $2
+                fi
+                return 0;
+        else
+                echo "false"
+                exit 1;
+  fi
+}
+
 if [ -n "$CONFIG_URL" ]; then
-    echo "Load file by url: ${CONFIG_URL}"
-    wget -O ${LOADER_BALANCER_CONFIG} $CONFIG_URL
+  echo "Load file by url: ${CONFIG_URL}"
+  if [ -n "$REPOUSR" ]  &&  [ -n "$REPOPWRD" ]; then
+  		USR="--user=$REPOUSR"
+  		PASS="--password=$REPOPWRD"
+  fi
+   URL="$ENVCONFURL $USR $PASS"
+   download_conf $URL $LOADER_BALANCER_CONFIG
 fi
